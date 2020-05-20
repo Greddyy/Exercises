7008aff4260d63ca0a917a018177282a1387af9b
Greddyy <55572482+Greddyy@users.noreply.github.com>
2020-05-20 21:55:29 +0300

    added wrapper class

---
+++ InterfaceEx/src/main/java/Wrapper.java
@@ -0,0 +1,16 @@
+public class Wrapper {
+
+    Transport transport;
+
+    public Wrapper(Transport transport) {
+        this.transport = transport;
+    }
+
+    public Transport getTransport() {
+        return transport;
+    }
+
+    public void setTransport(Transport transport) {
+        this.transport = transport;
+    }
+}
