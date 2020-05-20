41f8cd93473121183d3865eed8f973ab7a073954
Greddyy <55572482+Greddyy@users.noreply.github.com>
2020-05-20 22:00:41 +0300

    reverted changes

--- InterfaceEx/src/main/java/Car.java
+++ InterfaceEx/src/main/java/Car.java
@@ -1,18 +1,5 @@
 public class Car implements Transport {
-    Transport transport;
 
-    public Car(Transport transport) {
-        this.transport = transport;
-    }
-
-
-    public Transport getTransport() {
-        return transport;
-    }
-
-    public void setTransport(Transport transport) {
-        this.transport = transport;
-    }
 
     @Override
     public String moving() {
--- InterfaceEx/src/main/java/Plane.java
+++ InterfaceEx/src/main/java/Plane.java
@@ -1,18 +1,5 @@
 public class Plane implements Transport{
 
-    Transport transport;
-
-    public Plane(Transport transport) {
-        this.transport = transport;
-    }
-
-    public Transport getTransport() {
-        return transport;
-    }
-
-    public void setTransport(Transport transport) {
-        this.transport = transport;
-    }
 
     @Override
     public String moving() {
